5c9bff86706262178084b2a6f715279795f304ea
Dheera <dheera@ischool.berkeley.edu>
2013-11-03 21:16:56 -0800

    changed getJSON() to ajax({dataType: 'json'…})

--- js/script.js
+++ js/script.js
@@ -7,12 +7,7 @@ var allCameras = [];
 $(document).ready(function(){
     
 
-   
-
    console.log("In ready");
-
-
-
 
 
     // Add tags that a user types in
@@ -22,45 +17,66 @@ $(document).ready(function(){
            // var enddate = $("#enddatepicker").val() );
 
             console.log( "startdate = "+startdate );
+            var tagsWithSpaces;
 
             
-            var url = "http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=803b4fba97fed821c7d451d31da3c60f&date=" + startdate + "&per_page=500&format=json&jsoncallback=?";
+            var url = "http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=803b4fba97fed821c7d451d31da3c60f&date=" + startdate + "&extras=tags&per_page=500&format=json&jsoncallback=?";
                 
 
-            $.getJSON(url, function(data){
-                $.each(data.photos.photo, function(key,value){
+            $.ajax({
+               url: url,
+                dataType: 'json',
+                success: function(data){
 
-                    //console.log("Photo id = "+value.id);
+               $.each(data.photos.photo, function(key,value){
 
-                     //get tags for each photo
-                     var url1 = "http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=803b4fba97fed821c7d451d31da3c60f&photo_id="+value.id+"&format=json&jsoncallback=?" 
-                     $.getJSON(url1, function(data){
-                         $.each(data.photo.tags.tag, function(key,value){
+                                tagsWithSpaces=value.tags.trim();
+                                
+                                var tagsWithoutSpaces = tagsWithSpaces.split(' ');
+                                
+                                allTags.push(tagsWithoutSpaces);
+                               
 
-                            allTags.push(value._content);
+                            
 
+                                      //get camera info for each photo
+                                      var url2 = "http://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=803b4fba97fed821c7d451d31da3c60f&photo_id="+value.id+"&format=json&jsoncallback=?"
+                                      
+
+                                        $.ajax({
+                                           url: url2,
+                                            dataType: 'json',
+                                            success: function(data){
+
+                                                 $.each(data, function(key,value){
+                                                    if(typeof this.camera !== "undefined")
+                                                    {
+                                                        //console.log(this.camera);
+                                                     allCameras.push(this.camera);
+                                                    }
+                                                    
+                                                 });
+                                             }
+                                         });
+                                    
+                                 });
+
+                             }
                          });
 
-                          //get camera info for each photo
-                          var url2 = "http://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=803b4fba97fed821c7d451d31da3c60f&photo_id="+value.id+"&format=json&jsoncallback=?"
-                          $.getJSON(url2, function(data){
-                             
-                                 allCameras.push(data.photo.camera);
-
-                        
-                            });
-                        
-                     });
 
 
-                    interestingPhotos.push(value.id);
-                
-                });
+
+
+           
+            // return false;  
 
             //console.log( JSON.stringify(data) );
             }); 
             
-            return false;       
-        });
+
+        $.ajaxStop(function() {
+         console.log("Stopped");
+        });       
 
 });
