8b8d2cc9a38bfaaea187ad676735bff80721f164
dan tsai <tsai.dan@gmail.com>
2013-11-09 17:52:00 -0800

    haven't debugged yet...

--- js/script.js
+++ js/script.js
@@ -9,20 +9,27 @@ One set of JSON arrays per time period (month). Each array stored in a separate 
 
 Array of Photo objects (allPhotos):
 {
+	// the following variables are from the interestingness.getList call
 	id: 12345,
+	farm: 5,
+	server: 4444,
+	secret: 'e3d64kdk3',
+	title: 'sunset at the bridge'
+	tags: ["tag1","tag2","tag3"]
+
+	// the following variables are from the getExif call
+	// in my experience, make and model are inconsistent, perhaps to different file structures? i dunno. probably better to rely on the camera string than make+model
 	camera: "Canon 5D",
 	make: "Canon",
 	model: "5D",
-	tags: ["tag1","tag2","tag3"]
 }
 
-Array of Tag objects (allTags):
+Array of tags (allTags):
 {
-	tagLabel: "bridge",
+	tag: 'bridge',
 	count: 193,
 	photoIDs: [1234, 3841, 374, 173, 42853]
 }
-
 
 Array of Camera objects (allCameras):
 {
@@ -40,12 +47,39 @@ var allCameras = [];
 var ajaxConnections = 0;
 var apikey = '803b4fba97fed821c7d451d31da3c60f';
 
+// FlickrPhoto object constructor. Doesn't have the camera/make/model, which are added later
+function FlickrPhoto(id, farm, server, secret, title, tags) {
+	this.id = id;
+	this.farm = farm;
+	this.server = server;
+	this.secret = secret;
+	this.title = title;
+	this.tags = tags;
+	this.camera = null;
+	this.model = null;
+	this.make = null;
+}
+
+function FlickrTag(tag, count, photoid) {
+	this.tag = tag;
+	this.count = count;
+	this.photoIDs = [photoid];
+}
+
+function FlickrCamera(cameraString, make, model, count, photoid) {
+	this.camera = cameraString;
+	this.make = make;
+	this.model = model;
+	this.count = count;
+	this.photoIDs = [photoid];
+}
+
 $(document).ready(function(){
     // Add tags that a user types in
     $("#getInterestingnessPhotos").click(function(){
             console.log("Inside javascript");
             var startdate= $("#startdatepicker").val();
-           // var enddate = $("#enddatepicker").val() );
+            // var enddate = $("#enddatepicker").val() );
 
             console.log( "startdate = "+startdate );
 
@@ -61,9 +95,11 @@ $(document).ready(function(){
                     var cameraString, cameraMake, cameraModel;
 
 
-                    // add photos and tags into the allPhotos and allTags arrays
-                    // ADD CODE
-                    // ADD CODE
+                    // add photos and tags into the allPhotos array
+                    allPhotos.push(new FlickrPhoto(value.id, value.farm, value.server, value.secret, value.title, tagArray));
+
+                    // if tag doesn't exist in allTags, add it. if it exists, add photoID to photo list
+                    addPhotoTag(tagArray, value.id);
 
 					//get camera info for each photo
                     ajaxConnections++;
@@ -96,14 +132,18 @@ $(document).ready(function(){
 									cameraModel = data.photo.exif[i].tag;
 								}
 							}
+
+							// add cameras into the allCameras array. Also update the allPhotos array with camera information
+							if(data.photo.camera) {
+								addCameraToPhotos(value.id, data.photo.camera, cameraMake, cameraModel);
+							}
+
+							if(ajaxConnections==0) {
+								// finished with all requests, stringify
+								$("#allPhotosResult").text(JSON.stringify(allPhotos, null, " "));
+								$("#allTagsResult").text(JSON.stringify(allTags, null, " "));
+							}
 						}
-
-						// add cameras into the allCameras array. Also update the allPhotos array with camera information
-						// ADD CODE
-						// ADD CODE
-
-
-
 					})
 					.fail(function() {
 						console.log("flickr.photos.getExif fail for " + value.id);
@@ -113,66 +153,49 @@ $(document).ready(function(){
 			.fail(function() {
 				console.log("flickr.interestingness.getList fail");
 			});
-
-    }); 
+    });
 });
 
-/*
-            var url = "http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=803b4fba97fed821c7d451d31da3c60f&date=" + startdate + "&extras=tags&per_page=500&format=json&jsoncallback=?";
+// if tag doesn't exist in allTags, add it. if it exists, add photoID to photo list
+function addPhotoTag(tagArray, photoid) {
+	for(var i=0, j=tagArray.length ; i<j ; ++i) {
+		for(var k=0, m=allTags.length ; k<m ; ++k) {
+			if(allTags[k].tag == tagArray[i]) {
+				// found it!
+				allTags[k].count++;
+				allTags[k].photoIDs.push(tagArray[i]);
+			}
+			else if (k == (m-1)) {
+				// it's not in allTags (ie, it is not found, and we reached the last tag in allTags)
+				allTags.push(new FlickrTag(tagArray[i],1,photoid));
+			}
+		}
+		if(allTags.length == 0) {
+			// edge case -- nothing in allTags yet
+			allTags.push(new FlickrTag(tagArray[i],1,photoid));
+		}
+	}
+}
 
-            $.ajax({
-				beforesend: function(xhr) {
-				    ajaxConnections++;
-				    console.log("beforesend1. aC = " + ajaxConnections);
-				},
-                url: url,
-                dataType: 'json',
-                success: function(data){
-                	ajaxConnections--;
-                	console.log("ajaxConnections = " + ajaxConnections);
+// add camera to allPhotos and allCameras
+function addCameraToPhotos(photoid, cameraString, make, model) {
+	// add to allPhotos. photoid should already exist from interestingness.getInfo
+	for(var i=0, j=allPhotos.length; i < j ; ++i) {
+		if(allPhotos[i].id == photoid) {
+			allPhotos[i].camera = cameraString;
+			allPhotos[i].make = make;
+			allPhotos[i].model = model;
+		}
+	}
 
-	                $.each(data.photos.photo, function(key,value){
-                        tagsWithSpaces=value.tags.trim();
-
-                        var tagsWithoutSpaces = tagsWithSpaces.split(' ');
-
-                        allTags.push(tagsWithoutSpaces);
-
-						//get camera info for each photo
-                        var url2 = "http://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=803b4fba97fed821c7d451d31da3c60f&photo_id="+value.id+"&format=json&jsoncallback=?"
-
-                        $.ajax({
-                        	beforesend: function(xhr) {
-                        		ajaxConnections++;
-                        		console.log("beforesend2. aC = " + ajaxConnections);
-                        	},
-                            url: url2,
-                            dataType: 'json',
-                            success: function(data){
-                            	ajaxConnections--;
-	                            $.each(data, function(key,value){
-		                            if(typeof this.camera !== "undefined") {
-		                                //console.log(this.camera);
-	                                    allCameras.push(this.camera);
-	                                }
-	                            });
-	                        },
-                			error: function (xhr, errDesc, exception) {
-	                    	    ajaxConnections--;
-								console.log("ajaxConnections = " + ajaxConnections);
-	                    	}
-                        });
-                                    
-                    }); //end each
-
-                }, // end success
-                error: function (xhr, errDesc, exception) {
-                	ajaxConnections--;
-                	console.log("ajaxConnections = " + ajaxConnections);
-                }
-            });
-
-            //console.log( JSON.stringify(data) );
-             }); 
- });
-*/
+	// if camera doesn't exist in allCameras, add it. if it exists, add photoID to photo list
+	for(var i=0, j=allCameras.length ; i<j ; ++i) {
+		if(allCameras[i].camera == cameraString) {
+			// found it!
+			allCameras[i].photoIDs.push(photoid);
+		} else if(i == (j-1)) {
+			// it's not in allCamera (ie, it is not found, and we reached the last camera in array allCameras
+			allCameras.push(new FlickrCamera(cameraString, make, model, 1, photoid));
+		}
+	}
+}
