754f479535490ca0cd8c20939b923bb67d9dda96
dan tsai <tsai.dan@gmail.com>
2013-11-13 00:09:10 -0800

    animations. error checking. camera graph

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 <h1>Flickr Interestingness Explorer</h1>
 
 <h3>Project Description</h3>
-<p></p>
+<p>Flickr has a secret "interestingness" algorithm that picks 500 photos a day that it determines as the most "interesting". This visualization allows you to explore the tags associated with these interesting photos, as well as the cameras they were taken by. You can select a month's worth of photos -- we grab over a thousand photos for the selected month (exact amount varies, as returned by the Flickr API), and display the top 15 tags, or top 12 cameras.</p>
 
 <h3>Team Members and Roles</h3>
-<p>Dan Tsai<br/>
-Dheera Prabhakar<br />
-Divya Menghani<br />
+<p>Dan Tsai - D3 work, working with Flickr API, JSON caching<br/>
+Dheera Prabhakar - JavaScripting, onclick behavior for bars and carousel<br />
+Divya Menghani - JavaScripting, creating some bar graph code in D3<br />
 Shaohan Chen - HTML/CSS</p>
 
 <h3>Technologies Used</h3>
--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var allPhotos = [];
 var allTags = [];
 var allCameras = [];
+var allPhotosWithCameras = [];
 var svg;
 var bottomPadding = 50;
 var w = 900;
@@ -19,75 +20,109 @@ $(document).ready(function() {
 	    var camerasFile = 'json/' + month + 'cameras.json';
 	    var ajaxConnections = 0;
 
-	    // grab the three json arrays
+	    if(!month) {
+			$('#error').fadeIn('slow');
+		} else {
+			$('#criteria i').fadeTo('slow', 1.0);
+			$('#error').fadeOut('slow');
 
-	    // after grabbing all 3 json files,
-	    // show the tag bar graph using tagsFile as input
-	    ajaxConnections = 3;
-		$.getJSON(photosFile,
-		function (data) { 
-			ajaxConnections--;
+		    // grab the three json arrays
 
-			allPhotos = data; // this should work; dont think i have to manually push everything to allPhotos through .each()
+		    // after grabbing all 3 json files,
+		    // show the tag bar graph using tagsFile as input
+		    ajaxConnections = 3;
 
-			if(ajaxConnections == 0) {
-				// all 3 have been retrieved
-				if(cleared == 1)
-					window.setTimeout(showTagGraph, 1000);
-				else
-					showTagGraph();
-			}
-		})
-		.fail(function() {
-			console.log("getPhotos fail");
-			--ajaxConnections;
-		});
+		    // get photos
+			$.getJSON(photosFile,
+			function (data) { 
+				ajaxConnections--;
 
-		$.getJSON(tagsFile,
-		function (data) { 
-			ajaxConnections--;
+				allPhotos = data; // this should work; dont think i have to manually push everything to allPhotos through .each()
 
-			allTags = data;
+				allPhotosWithCameras = allPhotos.filter(filterCameras);
+				if(ajaxConnections == 0) {
+					// all 3 have been retrieved
+					if(cleared == 1)
+						window.setTimeout(showTagGraph, 1000);
+					else
+						showTagGraph();
+				}
+			})
+			.fail(function() {
+				console.log("getPhotos fail");
+				--ajaxConnections;
+			});
 
-			if(ajaxConnections == 0) {
-				// all 3 have been retrieved
-				if(cleared == 1)
-					window.setTimeout(showTagGraph, 1000);
-				else
-					showTagGraph();
-			}
-		})
-		.fail(function() {
-			console.log("getPhotos fail");
-			--ajaxConnections;
-		});
+			// get tags
+			$.getJSON(tagsFile,
+			function (data) { 
+				ajaxConnections--;
 
-		$.getJSON(camerasFile,
-		function (data) { 
-			ajaxConnections--;
+				allTags = data;
 
-			allCameras = data;
+				if(ajaxConnections == 0) {
+					// all 3 have been retrieved
+					if(cleared == 1)
+						window.setTimeout(showTagGraph, 1000);
+					else
+						showTagGraph();
+				}
+			})
+			.fail(function() {
+				console.log("getPhotos fail");
+				--ajaxConnections;
+			});
 
-			if(ajaxConnections == 0) {
-				// all 3 have been retrieved
-				if(cleared == 1)
-					window.setTimeout(showTagGraph, 1000);
-				else
-					showTagGraph();
-			}
-		})
-		.fail(function() {
-			console.log("getPhotos fail");
-			--ajaxConnections;
-		});
+			// get cameras
+			$.getJSON(camerasFile,
+			function (data) { 
+				ajaxConnections--;
 
+				allCameras = data;
+				if(ajaxConnections == 0) {
+					// all 3 have been retrieved
+					if(cleared == 1)
+						window.setTimeout(showTagGraph, 1000);
+					else
+						showTagGraph();
+				}
+			})
+			.fail(function() {
+				console.log("getPhotos fail");
+				--ajaxConnections;
+			});
+		} // end error checking if
 	}); // end clickhandler
 
 	$("#show_viz-camera").click(function() {
-		clearSVG();
+	    var month = $("#startdatepicker").val();
+	    if(!month) {
+			$('#error').fadeIn('slow');
+		} else {
+			$('#criteria i').fadeTo('slow', 1.0);
+			$('#error').fadeOut('slow');
+			clearSVG();
+			window.setTimeout(showCameraGraph, 1000);
+		}
+	});
+
+	$('#show_viz').click(function() {
+	    var month = $("#startdatepicker").val();
+	    if(!month) {
+			$('#error').fadeIn('slow');
+		} else {
+			$('#criteria i').fadeTo('slow', 1.0);
+			$('#error').fadeOut('slow');
+			clearSVG();
+			window.setTimeout(showTagGraph, 1000);
+		}
 	});
 });
 
+// filtering function for allPhotos to remove photos without cameras
+function filterCameras(element) {
+	return element.camera;
+}
 
 function clearSVG() {
 	var bars = svg.selectAll("rect.bar");
@@ -107,15 +142,41 @@ function hide() {
 	});
 }
 
-//D3 - Copy the below fucntion
 function showTagGraph() {
-	var maxTagCount = d3.max(allTags, function(d) {
+	showGraph(allTags, 'tags');
+}
+
+function showCameraGraph() {
+	showGraph(allCameras, 'cameras');
+}
+
+// general graph. type is either tags or cameras
+function showGraph(arr, type) {
+	var maxCount = d3.max(arr, function(d) {
 		return d.count;
 	});
+	var tickSpacing;
+	var barCount;
+	var barWidth;
+	var barSpacing; // total spacing between bars
+	switch(type) {
+		case "tags":
+			tickSpacing = 50;
+			barCount = 15;
+			barWidth = 30;
+			barSpacing = 50;
+			break;
+		case "cameras":
+			tickSpacing = 20;
+			barCount = 12;
+			barWidth = 30;
+			barSpacing = 65;
+			break;
+	}
 	var tickValues = [];
 
 	// generate ticks
-	for(var i = 0 ; i < maxTagCount ; i = i+50) {
+	for(var i = 0 ; i < maxCount ; i = i+tickSpacing) {
 		tickValues.push(i);
 	}
 
@@ -124,11 +185,11 @@ function showTagGraph() {
 
 	// scaling the height of bar
 	var yScale = d3.scale.linear()
-		.domain([0, maxTagCount])
+		.domain([0, maxCount])
 		.range([0, h - bottomPadding]);
 
 	var yAxisScale = d3.scale.linear()
-		.domain([0, maxTagCount])
+		.domain([0, maxCount])
 		.range([h - bottomPadding, 0]);
 
 	var yAxis = d3.svg.axis()
@@ -145,7 +206,7 @@ function showTagGraph() {
 	for (var j=0; j < tickValues.length; j++) {
 	    svg.append("line")
 	    	.attr({
-		    	"x1": bottomPadding,
+		    	"x1": 80,
 		    	"x2": w,
 		    	"y1": yAxisScale(tickValues[j]),
 		    	"y2": yAxisScale(tickValues[j]),
@@ -153,75 +214,107 @@ function showTagGraph() {
 	    	});
 	};
 
+	// bars
 	var bars = svg.selectAll("rect")
-		.data(allTags.slice(0,15))
+		.data(arr.slice(0,barCount))
 		.enter().append("rect")
 		.attr("x", function(d, i) {
-			return i * 50 + 80;
+			return i * barSpacing + 80;
 		})
-		.attr("y", function(d) {
-			return h - bottomPadding - yScale(d.count);
-		})
-		.attr("width", 30)
+		.attr("width", barWidth)
+		.attr("height", 0)
+		.attr("y", h - bottomPadding)
 
+		.attr("class","bar")
+		.on("click",function(d){ 
+			// when clicking on a bar, populate the carousel with 10 photos from the interestingness list
+			$(".carousel-inner").empty();
+
+			for(var i=0;i< d.photoIDs.length; i++)
+			{
+				var id = d.photoIDs[i];
+
+				for (var j = 0; j < allPhotos.length; j++) {
+
+				    var object = allPhotos[j];
+				    
+				    if(id===object["id"]){
+				    	
+						var t_url = "http://farm" + object["farm"] + ".static.flickr.com/" + object["server"] + "/" + object["id"] + "_" + object["secret"] + "_" + "z.jpg";
+					    
+					    console.log(t_url);
+
+					    if(i==0)
+					    {
+							$(".carousel-inner").append('<div class="item active"><img src='+t_url+' alt="">');
+
+						}
+						else{
+							$(".carousel-inner").append('<div class="item"><img src='+t_url+' alt="">');
+						}			   
+					}
+
+				}
+			}
+		})
+		.transition()
+		.duration (500)
 		.attr("height", function(d) {
 			return yScale(d.count);
 		})
-		.attr("class","bar")
-		.on("click",function(d){ 
-			// start: added by dheera
-			$(".carousel-inner").empty();
 
-					for(var i=0;i< d.photoIDs.length; i++)
-					{
-						var id = d.photoIDs[i];
-
-						for (var j = 0; j < allPhotos.length; j++) {
-
-						    var object = allPhotos[j];
-						    
-						    if(id===object["id"]){
-						    	
-							var t_url = "http://farm" + object["farm"] + ".static.flickr.com/" + object["server"] + "/" + object["id"] + "_" + object["secret"] + "_" + "t.jpg";
-						    
-						    console.log(t_url);
-
-						    if(i==0)
-						    {
-								$(".carousel-inner").append('<div class="item active"><img src='+t_url+' alt=""><div class="carousel-caption"><h4>Title '+(i+1)+'</h4><p></p>');
-
-							}
-							else{
-								$(".carousel-inner").append('<div class="item"><img src='+t_url+' alt=""><div class="carousel-caption"><h4>Title '+(i+1)+'</h4><p></p>');
-							}			   
-						}
-
-					}
-				}
-//end: added by dheera
-
-	});
+		.attr("y", function(d) {
+			return h - bottomPadding - yScale(d.count);
+		})
+;
 
 	// text labels
-	svg.selectAll("text.barlabel")
-		.data(allTags.slice(0,15))
+	svg.selectAll("text.barLabel")
+		.data(arr.slice(0,barCount))
 		.sort()
 		.enter()
 		.append("text")
 		.text(function(d) {
-			if(d.tag == "") {
-				return "(none)";
-			} else {
-				return d.tag;
+			var label;
+			switch(type) {
+				case "tags":
+					label = d.tag;
+					break;
+				case "cameras":
+					label = d.camera;
+					break;
 			}
+			return (label == "") ? '(none)' : label;
 		})
 		.attr("text-anchor", "middle")
 		.attr("x", function(d, i) {
-			return i * 50 + 80 + 15;
+			return i * barSpacing + (barWidth / 2) + 80;
 		})
-		.attr("y", function(d) {
-			return h - 25;
+		.attr("y", function(d, i) {
+			// stagger cameras because they're longer names
+			if(type == "cameras") {
+				return (i % 2 == 1) ? h - 10 : h - 25;
+			} else {
+				return h - 25;
+			}
 		})
-		.attr("text-anchor", "middle")
-		.attr("class", "barlabel")
+		.attr("class", "barLabel");
+
+	// show total photo count in topright
+	var totalString;
+	if(type == "tags") {
+		// get total number of photos
+		totalString = "total number of photos: " + allPhotos.length;
+	} else if(type == "cameras") {
+		// get total number of photos with a valid camera (not all photos have camera exif info available)
+		totalString = "photos with camera info: " + allPhotosWithCameras.length;
+	}
+
+	svg.append("text")
+		.text(totalString)
+		.attr("text-anchor", "end")
+		.attr("x", w - 50)
+		.attr("y", 15)
+		.attr("class", "countLabel");
+
 }
